e328db4a8b5abb0339adf72e9a5ae71fa829f0e3
AFz <afz.badie@gmail.com>
2024-09-11 01:04:06 +0330

    Fix env variables loading

--- main.go
+++ main.go
@@ -69,9 +69,7 @@ func main() {
 }
 
 func run() error {
-	if err := godotenv.Load(); err != nil {
-		return fmt.Errorf("error loading .env file: %w", err)
-	}
+	_ = godotenv.Load()
 
 	spotifyClient, err := newSpotifyClient()
 	if err != nil {
